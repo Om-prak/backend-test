30700eb50de54722ba1feba33fd83118eb966644
prakash <prakash.v124421@gmail.com>
2024-03-12 12:57:25 +0530

    second

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,29 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
+
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('signup');
 });
+
+
+router.post('/', function(req, res, next) {
+
+  res.render('signup');
+
+  };
+
+router.get('/login', function(req, res, next) {
+  
+  res.render('login');
+  
+});
+
+router.post('/login', function(req, res, next) {
+
+  res.render('login');
+
+  };
+
 
 module.exports = router;
